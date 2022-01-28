d197e5b05e582682e20c46adc0406cba713ce474
Miles Burke <milesj76@gmail.com>
2022-01-28 13:33:19 -0500

    add results view selector

--- src/components/Results.svelte
+++ src/components/Results.svelte
@@ -3,11 +3,40 @@
 </script>
 
 <section>
-  <h2>Results</h2>
+  <div class="top-row">
+    <h2>Results</h2>
+    <div class="selector">
+      <div class="btn-selector" id="detail">Detail</div>
+      <div class="btn-selector" id="table">Table</div>
+    </div>
+  </div>
 </section>
 
 <style>
   section {
     width: 50%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .top-row {
+    /* width: 100%; */
+  }
+
+  .selector {
+    display: flex;
+  }
+  
+  .btn-selector {
+    border: 4px solid #444;
+    padding: 0.3rem 1rem;
+  }
+
+  #detail {
+    border-right: 2px solid #444;
+  }
+
+  #table {
+    border-left: 2px solid #444;
   }
 </style>
